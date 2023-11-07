ab8e9efb773b00b20093397669982146b8d40292
greenbird1 <487751965@qq.com>
2023-11-07 23:27:46 +0800

    feat: init axios

---
+++ src/service/index.ts
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import { requestType, requestMethod } from './type'
+
+const service = axios.create({
+  timeout: 10000,
+  baseURL: 'https://netease-cloud-music-api-ochre-iota-91.vercel.app',
+  withCredentials: true
+})
+
+const request: requestType & requestMethod = (url, method, data, config) => {
+  return service({
+    url,
+    method,
+    [method.toLocaleLowerCase() === 'get' ? 'params' : 'data']: data,
+    ...config,
+  })
+}
+request.get = (url, data, config) => {
+  return request(url, 'get', data, config)
+}
+request.post = (url, data, config) => {
+  return request(url, 'post', data, config)
+}
+request.delete = (url, data, config) => {
+  return request(url, 'delete', data, config)
+}
+request.patch = (url, data, config) => {
+  return request(url, 'patch', data, config)
+}
+
+export default request
---
+++ src/service/type.d.ts
@@ -0,0 +1,29 @@
+import type { Method, AxiosRequestConfig } from 'axios'
+
+export type ResponseData<T = any> = {
+  data: T
+  extra: any
+  message: string
+  status: number
+  success: boolean
+}
+
+// 总方法的声明
+export type requestType = <T = any>(
+  url: string,
+  method: Method,
+  data?: unknown,
+  config?: AxiosRequestConfig // 配置其他
+) => Promise<ResponseData<T>>
+
+// 单独方法的声明
+export type requestMethodType = <T = any>(
+  url: string,
+  data?: unknown,
+  config?: AxiosRequestConfig // 配置其他
+) => Promise<ResponseData<T>>
+
+// 直接生成每一个对应的简单的方法
+export type requestMethod = {
+  [k in 'get' | 'post' | 'delete' | 'patch']: requestMethodType
+}
