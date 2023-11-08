6ac516854a05d4d24c6f703c1382191b6581bb85
QiTao Tang <tangqitao@bluepulse.cn>
2023-11-08 16:05:34 +0800

    refactor: change the introduction of the picture

--- src/views/download/index.tsx
+++ src/views/download/index.tsx
@@ -1,4 +1,10 @@
 import type { FC, ReactNode } from 'react'
+import pcImg from '@/assets/images/download-pc.jpeg'
+import windowsImg from '@/assets/images/download-windows.png'
+import macImg from '@/assets/images/download-mac.png'
+import mobileImg from '@/assets/images/download-mobile.jpeg'
+import androidImg from '@/assets/images/download-android.png'
+import iosImg from '@/assets/images/download-ios.png'
 import DownloadWrapper from './index.styled'
 interface IProps {
   children?: ReactNode
@@ -9,39 +15,19 @@ const Download: FC<IProps> = () => {
     <DownloadWrapper>
       <div className="pc">
         <h3>在电脑上听</h3>
-        <img height="273" src="/src/assets/images/download-pc.jpeg" alt="pc" />
+        <img height="273" src={pcImg} alt="pc" />
         <div className="download-buttons">
-          <img
-            height="44"
-            src="/src/assets/images/download-windows.png"
-            alt="windows"
-          />
-          <img
-            height="44"
-            src="/src/assets/images/download-mac.png"
-            alt="mac"
-          />
+          <img height="44" src={windowsImg} alt="windows" />
+          <img height="44" src={macImg} alt="mac" />
         </div>
         <div className="download">下载电脑端</div>
       </div>
       <div style={{ marginLeft: '100px' }} className="mobile">
         <h3>在手机上听</h3>
-        <img
-          height="273"
-          src="/src/assets/images/download-mobile.jpeg"
-          alt="mobile"
-        />
+        <img height="273" src={mobileImg} alt="mobile" />
         <div className="download-buttons">
-          <img
-            height="44"
-            src="/src/assets/images/download-android.png"
-            alt="android"
-          />
-          <img
-            height="44"
-            src="/src/assets/images/download-ios.png"
-            alt="ios"
-          />
+          <img height="44" src={androidImg} alt="android" />
+          <img height="44" src={iosImg} alt="ios" />
         </div>
         <div className="download">下载手机端</div>
       </div>
