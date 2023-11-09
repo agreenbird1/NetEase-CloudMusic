1065ee2168622ef3f9d958b147778732704412aa
QiTao Tang <tangqitao@bluepulse.cn>
2023-11-09 11:50:30 +0800

    feat: store config

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore,  } from '@reduxjs/toolkit'
+import { TypedUseSelectorHook, useSelector, useDispatch } from "react-redux"
 
 export const store = configureStore({
     reducer: {},
@@ -8,3 +9,7 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>
 // 推断出类型: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
+
+
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
