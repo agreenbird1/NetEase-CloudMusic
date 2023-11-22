2324eb19da6cce4d1f4d850cab58bf29db365eac
greenbird1 <487751965@qq.com>
2023-11-22 23:12:24 +0800

    feat: add redux-persist

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
 import 'normalize.css'
 import '@/assets/css/reset.css'
 
 import router from './router'
 import { Provider } from 'react-redux'
-import { store } from './store'
+import { persistor, store } from './store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <RouterProvider router={router}></RouterProvider>
+    <PersistGate loading={null} persistor={persistor}>
+      <RouterProvider router={router}></RouterProvider>
+    </PersistGate>
   </Provider>
 )
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,28 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { TypedUseSelectorHook, useSelector, useDispatch } from 'react-redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import AuthSlice from './auth'
-import DiscoverySlice from "./discovery"
+import DiscoverySlice from './discovery'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
 
 export const store = configureStore({
   reducer: {
-    AuthSlice,
+    AuthSlice: persistReducer(persistConfig, AuthSlice),
     DiscoverySlice,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      //关闭redux序列化检测
+      serializableCheck: false,
+    }),
 })
+
+export const persistor = persistStore(store)
 
 // 从 store 本身推断出 `RootState` 和 `AppDispatch` 类型
 export type RootState = ReturnType<typeof store.getState>
