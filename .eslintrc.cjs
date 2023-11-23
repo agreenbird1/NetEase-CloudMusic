d1a2f8a82329003740cc3838ca3dbc2ac71a6e03
QiTao Tang <tangqitao@bluepulse.cn>
2023-11-23 15:33:32 +0800

    chore: forbid fast refresh

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,7 @@ module.exports = {
       { allowConstantExport: true },
     ],
     '@typescript-eslint/no-explicit-any': "off",
-    '@typescript-eslint/ban-ts-comment': "off"
+    '@typescript-eslint/ban-ts-comment': "off",
+    'react-refresh/only-export-components': "off"
   },
 }
