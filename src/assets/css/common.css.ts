448edddf13cbae0fc72416d071587a575f198528
greenbird1 <487751965@qq.com>
2023-12-26 23:06:04 +0800

    feat: add ellipsis

--- src/assets/css/common.css.ts
+++ src/assets/css/common.css.ts
@@ -20,6 +20,11 @@ const commonCss = {
       text-decoration: underline;
     }
   `,
+  ellipsis: css`
+    white-space: nowrap; /* 不换行 */
+    overflow: hidden; /* 溢出隐藏 */
+    text-overflow: ellipsis; /* 显示省略号 */
+  `,
 }
 
 export default commonCss
